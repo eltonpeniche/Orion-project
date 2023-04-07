38849fae71741a11891e5ff1161614728497dec7
eltonpeniche <elton_nfs@hotmail.com>
2023-04-07 19:19:06 -0300

    Class-based views Equipamento

--- apps/orion/views/equipamento.py
+++ apps/orion/views/equipamento.py
@@ -1,10 +1,24 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.http.response import Http404
+from django.shortcuts import get_object_or_404, redirect, render
+from django.utils.decorators import method_decorator
+from django.views import View
 from django.views.generic import ListView
 
 from apps.orion import utils
+from apps.orion.forms import EquipamentosForm
 from apps.orion.models import Equipamento
-from apps.usuarios.models import Usuario
 
 
+@login_required
+def equipamentos_select2(request, id):
+    dados = Equipamento.objects.filter(empresa=id)
+    dados_json = [{'id': d.id, 'nome': d.equipamento} for d in dados]
+    return JsonResponse(dados_json, safe=False)
+
+@method_decorator(login_required(login_url='usuarios:login' ,redirect_field_name='next'), name='dispatch' )
 class EquipamentoList(ListView):
     model = Equipamento
     context_object_name = "equipamentos"
@@ -21,4 +35,40 @@ class EquipamentoList(ListView):
         equipamento_page = utils.paginacao(self.request, ctx.get('equipamentos'))
         ctx.update({'equipamentos': equipamento_page })
         return ctx
+
+@method_decorator(login_required(login_url='usuarios:login' ,redirect_field_name='next'), name='dispatch' )
+class EquipamentoDetail(View):
     
+    def get_equipamento(self, id):
+        equipamento = None
+        if id:  
+            equipamento = get_object_or_404(Equipamento, pk=id )
+
+        return equipamento
+         
+
+    def render_equipamento(self, form, id ):
+        return render(self.request, 'orion/pages/detalhes_equipamentos.html', { 'form': form, 'id': id})
+
+    def get(self, request, id=None):
+        equipamento = self.get_equipamento(id)
+        equipamentoForm = EquipamentosForm(instance=equipamento)
+        return self.render_equipamento(equipamentoForm, id)
+
+    def post(self, request, id=None):
+        equipamento = self.get_equipamento(id)
+        equipamentoForm = EquipamentosForm(self.request.POST, instance=equipamento)
+        
+        if equipamentoForm.is_valid():
+            messages.success(self.request, "Equipamento Salvo com Sucesso")
+            equipamentoForm.save()
+            return redirect('orion:equipamentos')
+        else:
+            return self.render_equipamento(equipamentoForm, id)
+            
+@method_decorator(login_required(login_url='usuarios:login' ,redirect_field_name='next'), name='dispatch' )
+class EquipamentoDelete(EquipamentoDetail):
+    def post(self, request, id=None):
+        equipamento = self.get_equipamento(id)
+        equipamento.delete()
+        return redirect('orion:equipamentos')
